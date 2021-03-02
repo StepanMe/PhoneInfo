164eddcbadcd58d884f5fc5b0fea9a9deef8d105
Stepan Mednikov <x-steff@mail.ru>
2021-03-02 09:56:58 +0300

    Изменил метод проверки введённого номера. Теперь он принимает только цифры

--- app/src/main/java/com/example/phoneinfo/MainActivity.java
+++ app/src/main/java/com/example/phoneinfo/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.phoneinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -22,20 +24,18 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
     String phoneString;
 
-    EditText etPhone;
-    Button bSearch;
-    ImageButton bClear;
     TextView tvOperator;
     TextView tvRegion;
+
+    EditText etPhone;
+    ImageButton bClear;
+    Button bSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,16 +50,25 @@ public class MainActivity extends AppCompatActivity {
         tvRegion = findViewById(R.id.tv_region);
         bClear = findViewById(R.id.b_clear);
 
+
         View.OnClickListener searchClick = view -> {
-            phoneString = etPhone.getText().toString();
+            // Скрываем клавиатуру
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(bSearch.getWindowToken(),inputMethodManager.HIDE_NOT_ALWAYS);
+
+            // Очищаем строку с номером телефона от ненужных символов, оставляем только цифры
+            phoneString = etPhone.getText().toString().replaceAll("[^0-9]","");;
+            // Если строка пустая, просим пользователя ввести номер телефона
             if (phoneString.length() == 0) {
                 Toast.makeText(this,R.string.toast_error_enter_correct_phone,Toast.LENGTH_LONG).show();
                 return;
             }
-            String requestUrl = "https://num.voxlink.ru/get/?num=" + phoneString.replaceAll("[^+0-9]","");
+            // Формируем URL (строку) для запроса
+            String requestUrl = "https://num.voxlink.ru/get/?num=" + phoneString;
             RequestQueue requestQueue = Volley.newRequestQueue(this);
 
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, requestUrl, null, new Response.Listener<JSONObject>() {
+                // В случае успеха
                 @Override
                 public void onResponse(JSONObject response) {
                     GsonBuilder gsonBuilder = new GsonBuilder();
@@ -70,18 +79,19 @@ public class MainActivity extends AppCompatActivity {
                     tvRegion.setText(String.format(getString(R.string.tv_region_title),p.getRegion()));
                 }
             }, new Response.ErrorListener() {
+                // В случае ошибки
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     // Если сервер вернул ответ с кодом 404, то это не совсем ошибка
                     // Просто сообщаем, что номер введён неправильно или номер не найден
                     if ("404".equals(String.valueOf(error.networkResponse.statusCode))) {
-                        String errResponseBody = null;
-                        //Пробуем преобразовать содержимое ответа сервера из массива byte'ов в строку
+                        String errResponseBody;
+                        //Преобразуем содержимое ответа сервера из массива byte'ов в строку
                         errResponseBody = new String(error.networkResponse.data, StandardCharsets.UTF_8);
 
                         GsonBuilder gsonBuilder = new GsonBuilder();
                         Gson gson = gsonBuilder.create();
-                        // Готовим сообщение для тоста
+                        // Готовим текст сообщения для Toast'а
                         String toastMessage;
                         PhoneError phoneError = gson.fromJson(errResponseBody,PhoneError.class);
                         switch (phoneError.ErrorType()) {
@@ -91,7 +101,13 @@ public class MainActivity extends AppCompatActivity {
                                 break;
                             default: toastMessage = getString(R.string.toast_error_wrong_format_or_not_found);
                         }
+
                         Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+
+                        // Если возникла такая ошибка, очищаем результаты возможного предыдущего корректного запроса
+                        tvOperator.setText("");
+                        tvRegion.setText("");
+
                         Log.i("asd123","Содержимое ответа: " + errResponseBody);
                     // Если код ответа отличается от 404, действительно, какая-то ошибка
                     // Сообщаем об этом пользователю
@@ -99,14 +115,13 @@ public class MainActivity extends AppCompatActivity {
                         String toastMessage;
                         if (error instanceof TimeoutError) {
                             toastMessage = getString(R.string.toast_error_server_response_timeout);
-//                            Toast.makeText(MainActivity.this, "Сервер долго отвечает", Toast.LENGTH_LONG).show();
                             Log.i("asd123", "Сервер  долго отвечает: " + error.toString());
                         } else if (error instanceof NoConnectionError || error instanceof ServerError) {
                             toastMessage = getString(R.string.toast_error_no_internet_connection);
-//                            Toast.makeText(MainActivity.this, "Нет соединения с интернетом", Toast.LENGTH_LONG).show();
                             Log.i("asd123", "Нет соединения с сервером: " + error.toString());
                         } else {
                             toastMessage = getString(R.string.toast_error_unknown);
+                            Log.i("asd123", "Ошибка в ответе сервера: " + error.toString());
                         }
                         Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG).show();
                     }
@@ -116,10 +131,14 @@ public class MainActivity extends AppCompatActivity {
         };
 
         View.OnClickListener clearClick = view -> {
+            // Очищаем название оператора, название региона и поле поиска
             tvOperator.setText("");
             tvRegion.setText("");
             etPhone.setText("");
-            etPhone.setSelection(0);
+
+            // Показываем клавиатуру
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.showSoftInput(etPhone,0);
         };
 
         bClear.setOnClickListener(clearClick);
